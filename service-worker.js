123ad0b588ff3143850b207d2a6107446f16303a
SoftWeekStudio <softweekstudio@gmail.com>
2026-09-01 12:52:57 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "pet-care-organizer-v2-1-pwa";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./favicon-32x32.png",
+  "./apple-touch-icon-180x180.png",
+  "./icon-192x192.png",
+  "./icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
